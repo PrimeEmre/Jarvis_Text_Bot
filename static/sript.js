a75733ba770f8eaaf2fa14d8c9d0d1116c865532
PrimeEmre <ynsemrgzl@gmail.com>
2026-03-05 12:00:50 -0500

    Setup neesary UI compoments

--- static/sript.js
+++ static/sript.js
@@ -1,3 +1,13 @@
-function hello(){
-alert("hello")
-}
+// setting bar and charter count 
+
+document.getElementById("text-input").addEventListener("input" , function(){
+ document.getElementById("char-count").textContent = this.value.length 
+})
+
+// setting the speed 
+document.getElementById("rate").addEventListener("input", function() {
+document.getElementById("rate-value").textContent = parseFloat(this.value).toFixed(1) + "x" 
+})
+
+// setting the main button 
+document.getElementById("action-btn").addEventListener("click" ,SpeechSynthesis)
